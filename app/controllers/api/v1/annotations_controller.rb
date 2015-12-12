c2ca0365b7d7607ed80265221eaeb57e8b503340
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-12-12 18:59:32 -0500

    added CORS support for put and delete options

--- app/controllers/api/v1/annotations_controller.rb
+++ app/controllers/api/v1/annotations_controller.rb
@@ -2,20 +2,16 @@ class Api::V1::AnnotationsController < ApplicationController
   # respond_to :json
 
   def index
-    # binding.pry
     render json: Annotation.all
   end
 
   def show
-    binding.pry
-    # annotations = Readme.find(params[:readme_id]).annotations
     annotations = Annotation.where(uri: request.referrer)
     results = {total: annotations.size, rows: annotations}
     render json: results
   end
 
   def create
-    binding.pry
     annotation = Annotation.create(annotation_params)
     annotation.update(uri: request.referrer)
     render json: Annotation.create(annotation_params)
@@ -30,7 +26,7 @@ class Api::V1::AnnotationsController < ApplicationController
   end
 
   def search
-    binding.pry
+    # binding.pry
   end
 
   private
